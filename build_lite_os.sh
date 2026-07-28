32ec687ccb322e0071ffb4663edb2e7cc87d4776
Balaji Pothula <balan.pothula@gmail.com>
2026-07-28 20:02:34 +0530

    Update build_lite_os.sh
    Added zlib and dropbear

--- build_lite_os.sh
+++ build_lite_os.sh
@@ -5,12 +5,13 @@
 SYSROOT="/var/tmp/sysroot"
 DL="$SYSROOT/dl"
 
-# Toybox package details.
+# Toybox, Linux Kernel, Zlib and Dropbear package details.
 toybox_tar="toybox-0.8.13.tar.gz"
-toybox_src="toybox-0.8.13"
-
-# Linux Kernel package details.
 linux_tar="linux-6.12.79.tar.xz"
+zlib_tar="zlib-1.3.1.tar.gz"
+dropbear_tar="dropbear-2024.86.tar.bz2"
+
+toybox_src="toybox-0.8.13"
 linux_src="linux-6.12.79"
 
 # Extract the Toybox source.
@@ -21,6 +22,10 @@ tar -xf "$DL/$linux_tar" -C "$DL" || exit 1
 
 # Switch to the Toybox source tree.
 cd "$DL/$toybox_src" || exit 1
+
+# Move Zlib and Dropbear packages into Toybox source.
+mv "$DL/$zlib_tar"     "$DL/$toybox_src"
+mv "$DL/$dropbear_tar" "$DL/$toybox_src"
 
 # Create the minimal Toybox configuration.
 cat > "$DL/$toybox_src/lite_os.config" << 'EoC'
@@ -38,4 +43,3 @@ KCONFIG_ALLCONFIG="$DL/$toybox_src/lite_os.config" make defconfig >/dev/null || 
   KEXTRA='CRYPTO,CRYPTO_AES,CRYPTO_HASH,CRYPTO_HMAC,CRYPTO_SHA1,CRYPTO_SHA256,DEVPTS_FS,EPOLL,FUTEX,HYPERVISOR_GUEST,IP_PNP,IP_PNP_DHCP,KVM_GUEST,PARAVIRT,SHMEM,UNIX98_PTYS,VIRTIO,VIRTIO_BLK,VIRTIO_MMIO,VIRTIO_MMIO_CMDLINE_DEVICES,VIRTIO_NET,VIRTIO_PCI' \
   OUTPUT="$DL/lite_os" \
   dropbear >/dev/null || exit 1
-
